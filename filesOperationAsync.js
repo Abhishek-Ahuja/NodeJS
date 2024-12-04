9131d8ab7884c4a529aa73b79f3f851ab908b7a3
Abhishek <abhishekahuja576@gmail.com>
2024-12-04 07:13:17 +0530

    async files operation

--- filesOperationAsync.js
+++ filesOperationAsync.js
@@ -3,13 +3,29 @@ const fs = require('fs');
 
 console.log("Started reading the file.");
 
-fs.readFile("data.txt", (err, data) => {
+fs.readFile("orderAsync.txt", (err, data) => {
     if(err){
         console.log(err.message)
     }else{
-        console.log(data.toString())
+        console.log("Data:",data.toString())
     }
 })
 
+
+console.log("Operations");
+// // To create and add data in order.txt
+// fs.writeFile("orderAsync.txt", "i: 1, type: image, quantiiy: 2", (err) => {
+//     if(err)console.log(err)
+// })
+
+//To append data in order.txt
+// fs.appendFile("orderAsync.txt", 'i: 2, type: choclate, quantiiy: 7', (err) => {
+//     if(err)console.log(err)
+// })
+
+//To delete file
+fs.unlink("orderAsync.txt", (err) => {
+    if(err)console.log(err)
+})
 
 console.log("Ended reading the file.");
