81c3d2132cdbb1018fd563b81e030a460321fe06
Abhishek <abhishekahuja576@gmail.com>
2024-12-05 06:38:03 +0530

    get api is also added

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const server = http.createServer((request, response) => {
        return response.end(", Welcome to my NodeJS server.")
    }else if(request.url === '/about'){
        return response.end(", I am working as a full stack developer in Wipro Technologies.")
-   }
+   }else if(request.method == 'POST'){
+    let body = '';
+    request.on('data', (chunk) => {
+        body+= chunk.toString();
+    })
+    request.on('end', () => {
+        console.log('Data received.', body);
+        return response.end(body)
+    })
+   }else
    response.end(", Response received at port 8080.")
 })
 
